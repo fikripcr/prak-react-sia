64e80427de29b069167684b5dd3cced799a4c406
fikripcr <fikri@pcr.ac.id>
2026-04-28 08:02:58 +0700

    Pertemuan 7: Penerapan AuthLayout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Orders from "./pages/Orders";
 import Customers from "./pages/Customers";
 import NotFound from "./pages/NotFound";
 import MainLayout from "./layouts/MainLayout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+import AuthLayout from "./layouts/AuthLayout";
 
 function App() {
 
@@ -15,6 +19,12 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/orders" element={<Orders />} />
             <Route path="/customers" element={<Customers />} />
+        </Route>
+
+        <Route element={<AuthLayout/>}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/forgot" element={<Forgot/>} />
         </Route>
 
     </Routes>
